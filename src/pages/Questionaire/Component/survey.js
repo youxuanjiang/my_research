bcb39403d36977368eb63af2db2f8b064be3c1c4
youxuan <youxuanjiang19960901@gmail.com>
2022-06-22 18:12:22 +0800

    update suvey question

--- src/pages/Questionaire/Component/survey.js
+++ src/pages/Questionaire/Component/survey.js
@@ -4,23 +4,23 @@ const Survey = ({plausibility, setPlausibility, urgency, setUrgency, importance,
   // 3. 若要回答上述情境中的問題，您認為回答者所感受到的麻煩程度為何？
   const questionList = [
     <div>
-      <p className="surveyQuestion"> 1. 依照上述情境，即時資訊的合理性如何？</p>
+      <p className="surveyQuestion"> 1. 上述情境中，即時資訊的合理性如何？</p>
       <div>
-        毫無道理
+        非常不合理
         <input
           type="radio"
-          checked={plausibility === 7}
-          onChange={() => setPlausibility(7)}
+          checked={plausibility === 1}
+          onChange={() => setPlausibility(1)}
         />
         <input
           type="radio"
-          checked={plausibility === 6}
-          onChange={() => setPlausibility(6)}
+          checked={plausibility === 2}
+          onChange={() => setPlausibility(2)}
         />
         <input
           type="radio"
-          checked={plausibility === 5}
-          onChange={() => setPlausibility(5)}
+          checked={plausibility === 3}
+          onChange={() => setPlausibility(3)}
         />
         <label> - </label>
         <input
@@ -31,26 +31,26 @@ const Survey = ({plausibility, setPlausibility, urgency, setUrgency, importance,
         <label> - </label>
         <input
           type="radio"
-          checked={plausibility === 3}
-          onChange={() => setPlausibility(3)}
+          checked={plausibility === 5}
+          onChange={() => setPlausibility(5)}
         />
         <input
           type="radio"
-          checked={plausibility === 2}
-          onChange={() => setPlausibility(2)}
+          checked={plausibility === 6}
+          onChange={() => setPlausibility(6)}
         />
         <input
           type="radio"
-          checked={plausibility === 1}
-          onChange={() => setPlausibility(1)}
+          checked={plausibility === 7}
+          onChange={() => setPlausibility(7)}
         />
         非常合理
       </div>
     </div>,
     <div>
-      <p className="surveyQuestion"> 2. 依照上述情境，您認為還剩下多少確定答案的時間呢？</p>
+      <p className="surveyQuestion"> 2. 您在上述情境中的時間是否充裕？</p>
       <div>
-        快來不及
+        非常不充裕
         <input
           type="radio"
           checked={urgency === 7}
@@ -88,66 +88,22 @@ const Survey = ({plausibility, setPlausibility, urgency, setUrgency, importance,
           checked={urgency === 1}
           onChange={() => setUrgency(1)}
         />
-        還很充裕
+        非常充裕
       </div>
     </div>,
     <div>
-      <p className="surveyQuestion"> 3. 在上述情境中，您認為若沒有順利解決問題所造成的代價有多嚴重呢？</p>
-      <div>
-        毫無影響
-        <input
-          type="radio"
-          checked={importance === 1}
-          onChange={() => setImportance(1)}
-        />
-        <input
-          type="radio"
-          checked={importance === 2}
-          onChange={() => setImportance(2)}
-        />
-        <input
-          type="radio"
-          checked={importance === 3}
-          onChange={() => setImportance(3)}
-        />
-        <label> - </label>
-        <input
-          type="radio"
-          checked={importance === 4}
-          onChange={() => setImportance(4)}
-        />
-        <label> - </label>
-        <input
-          type="radio"
-          checked={importance === 5}
-          onChange={() => setImportance(5)}
-        />
-        <input
-          type="radio"
-          checked={importance === 6}
-          onChange={() => setImportance(6)}
-        />
-        <input
-          type="radio"
-          checked={importance === 7}
-          onChange={() => setImportance(7)}
-        />
-        非常嚴重
-      </div>
-    </div>,
-    <div>
-      <p className="surveyQuestion"> 4. 假設你現在能透過app去請任何能幫助您的人回答問題的話，您會希望他提供怎樣的資訊？</p>
+      <p className="surveyQuestion"> 3. 假設你現在能透過app去請任何能幫助您的人回答問題的話，您會希望他提供怎樣的資訊？</p>
       <div>
         <input
           type="radio"
           checked={crowdsourcingType.trim() === 'VERIFICATION'}
           onChange={() => setCrowdsourcingType('VERIFICATION')}
-        />請對方單純幫我<span className="highLine">驗證該資訊</span>的真偽<br/>
+        />請對方告訴我現場的<span className="highLine">實際情況</span><br/>
         <input
           type="radio"
           checked={crowdsourcingType.trim() === 'COMPLEMENT'}
           onChange={() => setCrowdsourcingType('COMPLEMENT')}
-        />請對方告訴我<span className="highLine">為什麼資訊會有差別</span><br/>
+        />請對方告訴我現場的<span className="highLine">實際情況</span>以及<span className="highLine">原因</span><br/>
         <input
           type="radio"
           checked={crowdsourcingType.trim() === 'NONE'}
@@ -157,7 +113,7 @@ const Survey = ({plausibility, setPlausibility, urgency, setUrgency, importance,
       </div>
     </div>,
     <div>
-      <p className="surveyQuestion"> 5. 若要回答您想詢問的問題，您認為回答者會覺得多麻煩呢？</p>
+      <p className="surveyQuestion"> 4. 若要回答您想詢問的問題，您認為回答者會覺得多麻煩呢？</p>
       <div>
         非常容易
         <input
@@ -201,7 +157,7 @@ const Survey = ({plausibility, setPlausibility, urgency, setUrgency, importance,
       </div>
     </div>,
     <div>
-      <p className="surveyQuestion"> 6. 您能透過給予現金的方式，讓任何能幫助您的人可以<span className="highLine">立即</span>提供資訊的話，根據上述情境，您願意提供多少新台幣呢？</p>
+      <p className="surveyQuestion"> 5. 您能透過給予現金的方式，讓任何能幫助您的人可以<span className="highLine">立即</span>回答您的問題的話，根據上述情境，您願意提供多少新台幣呢？</p>
       <div>
         0
         <input
